36ecd1a594e56f870e33f07a9dc75cb9816bee1b
Carlareneedc <carla.renee.diazc@gmail.com>
2020-12-08 11:56:54 -0300

    listo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { LineChart, PieChart, ColumnChart} from 'react-chartkick'
+import 'chart.js'
+import axios from "axios"
 
+
+fetch('https://mindicador.cl/api')
+.then(response => response.json())
+.then(json => console.log(json))
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="indi"><h1>Indicadores Económicos</h1>
+    <LineChart data={{"2020-12-03T03:00:00.000Z": 755.34, "2017-05-14": 5}} />,
+    <ColumnChart data={[["Sun", 32], ["Mon", 46], ["Tue", 28]]} />
     </div>
   );
 }
---
+++ src/Grafico.jsx
@@ -0,0 +1,16 @@
+import { LineChart, PieChart } from 'react-chartkick'
+import 'chart.js'
+
+
+function Grafico() {
+    return (
+     
+       
+        <GeoChart data={[["United States", 44], ["Germany", 23], ["Brazil", 22]]} />
+   
+     
+    );
+  }
+  
+  export default Grafico;
+  
